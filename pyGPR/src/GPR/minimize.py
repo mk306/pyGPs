236b71fdfb4bd19637b06e75f1341a082f2f4a55
mathDR <dan.marthaler@gmail.com>
2012-10-16 16:56:17 -0700

    Fixed some derivative bugs; Added BFGS Minimization
    Fixed (and verifiied numerically) most covariance gradients (w.r.t.
    hyperparameters).  Furthermore, added scipy.optimize.fmin_bfgs
    minimization in lieu of minimize.run

--- pyGPR/src/GPR/minimize.py
+++ pyGPR/src/GPR/minimize.py
@@ -55,7 +55,7 @@ def run(X, f, grad, args, maxnumlinesearch=None, maxnumfuneval=None, red=1.0, ve
     #Tuning of SIG (depending on the nature of the function to be optimized) may
     #speed up the minimization; it is probably not worth playing much with RHO.
 
-    SMALL = 10.**-16 #minimize.m uses matlab's realmin
+    SMALL = 10.**-16 # A small number
     
     if maxnumlinesearch == None:
         if maxnumfuneval == None:
@@ -73,7 +73,9 @@ def run(X, f, grad, args, maxnumlinesearch=None, maxnumfuneval=None, red=1.0, ve
     i = 0            # zero the run length counter
     ls_failed = 0    # no previous line search has failed
     f0 = f(X, *args) # get function value and gradient
+    print "Function call: ",f0,X
     df0 = grad(X, *args)
+    print "Gradient call: ",df0
     fX = [f0]
     if length < 0: 
         i = i + 1 # count epochs
@@ -85,43 +87,46 @@ def run(X, f, grad, args, maxnumlinesearch=None, maxnumfuneval=None, red=1.0, ve
         if length > 0: 
             i = i + 1 # count iterations
 
-        X0 = X; F0 = f0; dF0 = df0 # make a copy of current values
+        X0 = X[:]; F0 = f0[:]; dF0 = df0[:] # make a copy of current values
         if length > 0:
             M = MAX
         else:
             M = min(MAX, -length-i)
-        while 1: # keep extrapolating as long as necessary
+
+        while True: # keep extrapolating as long as necessary
             x2 = 0; f2 = f0; d2 = d0; f3 = f0; df3 = df0
-            success = 0
+            success = False
             while (not success) and (M > 0):
                 try:
                     M = M - 1 
                     if length < 0: 
                         i = i + 1 # count epochs?!
                     f3 = f(X+x3*s, *args)
+                    print "Function call: ",f3,X+x3*s
                     df3 = grad(X+x3*s, *args)
+                    print "Gradient call: ",df0
                     if isnan(f3).any(1) or isinf(f3).any(1) or isnan(df3).any(1) or isinf(df3).any(1):
                         raise "Error. Function returning either inf or nan."
-                    success = 1
+                    success = True
                 except: # catch any error which occured in f
-                    x3 = (x2+x3)/2 # bisect and try again
+                    x3 = (x2+x3)/2. # bisect and try again
             if f3 < F0:
                 X0 = X+x3*s; F0 = f3; dF0 = df3 # keep best values
             d3 = dot(df3.T,s) # new slope
             if d3 > SIG*d0 or f3 > f0+x3*RHO*d0 or M == 0:
-                                                   # are we done extrapolating?
+                # are we done extrapolating?
                 break
             x1 = x2; f1 = f2; d1 = d2 # move point 2 to point 1
             x2 = x3; f2 = f3; d2 = d3 # move point 3 to point 2
-            A = 6*(f1-f2)+3*(d2+d1)*(x2-x1) # make cubic extrapolation
-            B = 3*(f2-f1)-(2*d1+d2)*(x2-x1)
-            Z = B+sqrt(complex(B*B-A*d1*(x2-x1)))
+            A = 6.*(f1-f2)+3.*(d2+d1)*(x2-x1) # make cubic extrapolation
+            B = 3.*(f2-f1)-(2*d1+d2)*(x2-x1)
+            Z = B + sqrt(complex(B*B-A*d1*(x2-x1)))
             if Z != 0.0:
                 x3 = x1-d1*(x2-x1)**2/Z # num. error possible, ok!
             else:
                 x3 = inf
             if (not isreal(x3)) or isnan(x3) or isinf(x3) or (x3 < 0):
-                                                       # num prob | wrong sign?
+                # num prob | wrong sign?
                 x3 = x2*EXT # extrapolate maximum amount
             elif x3 > x2*EXT: # new point beyond extrapolation limit?
                 x3 = x2*EXT # extrapolate maximum amount
@@ -130,28 +135,27 @@ def run(X, f, grad, args, maxnumlinesearch=None, maxnumfuneval=None, red=1.0, ve
             x3 = real(x3)
 
         while (abs(d3) > -SIG*d0 or f3 > f0+x3*RHO*d0) and M > 0:
-                                                           # keep interpolating
+            # keep interpolating
             if (d3 > 0) or (f3 > f0+x3*RHO*d0): # choose subinterval
                 x4 = x3; f4 = f3; d4 = d3 # move point 3 to point 4
             else:
                 x2 = x3; f2 = f3; d2 = d3 # move point 3 to point 2
             if f4 > f0:
-                x3 = x2-(0.5*d2*(x4-x2)**2)/(f4-f2-d2*(x4-x2))
-                                                      # quadratic interpolation
+                x3 = x2-(0.5*d2*(x4-x2)**2)/(f4-f2-d2*(x4-x2)) # quadratic interpolation
             else:
-                A = 6*(f2-f4)/(x4-x2)+3*(d4+d2) # cubic interpolation
-                B = 3*(f4-f2)-(2*d2+d4)*(x4-x2)
+                A = 6.*(f2-f4)/(x4-x2)+3.*(d4+d2) # cubic interpolation
+                B = 3.*(f4-f2)-(2*d2+d4)*(x4-x2)
                 if A != 0:
-                    x3=x2+(sqrt(B*B-A*d2*(x4-x2)**2)-B)/A
-                                                     # num. error possible, ok!
+                    x3=x2+(sqrt(B*B-A*d2*(x4-x2)**2)-B)/A # num. error possible, ok!
                 else:
                     x3 = inf
             if isnan(x3) or isinf(x3):
-                x3 = (x2+x4)/2 # if we had a numerical problem then bisect
-            x3 = max(min(x3, x4-INT*(x4-x2)),x2+INT*(x4-x2))
-                                                       # don't accept too close
+                x3 = (x2+x4)/2. # if we had a numerical problem then bisect
+            x3 = max(min(x3, x4-INT*(x4-x2)),x2+INT*(x4-x2)) # don't accept too close
             f3 = f(X+x3*s, *args)
+            print "Function call: ",f3,X+x3*s
             df3 = grad(X+x3*s, *args)
+            print "Gradient call: ",df0
             if f3 < F0:
                 X0 = X+x3*s; F0 = f3; dF0 = df3 # keep best values
             M = M - 1
@@ -169,14 +173,14 @@ def run(X, f, grad, args, maxnumlinesearch=None, maxnumfuneval=None, red=1.0, ve
             if d0 > 0: # new slope must be negative
                 s = -df0; d0 = -dot(s.T,s) # otherwise use steepest direction
             x3 = x3 * min(RATIO, d3/(d0-SMALL)) # slope ratio but max RATIO
-            ls_failed = 0 # this line search did not fail
+            ls_failed = False # this line search did not fail
         else:
             X = X0; f0 = F0; df0 = dF0 # restore best point so far
             if ls_failed or (i > abs(length)):# line search failed twice in a row
                 break # or we ran out of time, so we give up
             s = -df0; d0 = -dot(s.T,s) # try steepest
-            x3 = 1/(1-d0)
-            ls_failed = 1 # this line search failed
+            x3 = 1./(1.-d0)
+            ls_failed = True # this line search failed
     if ls_failed:
         print "Two line searches in a row failed..."
     if verbose: print "\n"
